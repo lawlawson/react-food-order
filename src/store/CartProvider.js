ba794ad478cc8ff20efa45bcbad6318bfb52273c
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-07-19 23:29:34 +0100

    Completed reducer functionality

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,6 +7,15 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
@@ -19,7 +28,10 @@ const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: 'ADD', item: item });
   };
-  const removeItemFromCartHandler = (id) => {};
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
